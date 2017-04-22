3af3584453c697878efd5486d6e4c44078bd95db
Daniel Sun <sunrentao@gmail.com>
2017-04-21 22:25:33 -0700

    add python file

---
+++ algo_trading.py
@@ -0,0 +1,14 @@
+# read data -------------------------------------
+import os 
+import scipy.io
+os.chdir(os.path.abspath('.')) # set working dictionary
+
+#data = scipy.io.loadmat("Data/CSCO_20141103.mat")
+data = scipy.io.loadmat("FB_20141103.mat")
+#
+dt = data["data"][0,0]
+listName=['Event','SellVolume','SellPrice',
+           'BuyVolume','BuyPrice']
+#
+for i in range(len(listName)):
+    exec(listName[i] + ' = dt[' + str(i) + ']')
